0d1aefec6376c5d729c9557a39d918a2aa7187d9
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-10-02 09:53:37 +0100

    oef

---
+++ ouf1.js
@@ -0,0 +1,12 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let getal1 = parseFloat(await userInput.question('Geef hierin je eerste getal: '));
+let getal2 = parseFloat(await userInput.question('Geef hierin je tweede getal: '));
+
+if(getal1 < getal2){
+    console.log('Het grootste getal is ' + getal1);
+}else{
+    console.log('Het grootste getal is ' + getal2);
+}
