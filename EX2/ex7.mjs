6045410bb40387c32405cf87f56e4acbb4458484
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-10-04 11:59:58 +0100

    updated ex7

--- EX2/ex7.mjs
+++ EX2/ex7.mjs
@@ -4,16 +4,16 @@ const userInput = readline.createInterface({input, output});
 
 
 let taal = await userInput.question('Please choose a language out of these 3: \n FR\n NL\n ENG\n: ');
-let FR ='FR';
-let NL ='NL';
-let ENG ='ENG';
+//let FR ='FR';
+//let NL ='NL';
+//let ENG ='ENG';
 
 
-if(taal == FR){
+if(taal == 'FR'){
     console.log('Bonjour cher visiteur, bienvenu sur notre site.');
-}else if(taal == NL){
+}else if(taal == 'NL'){
     console.log('Hallo beste bezoeker, welkom op onze site. ');    
-}else if(taal == ENG){
+}else if(taal == 'ENG'){
     console.log('Hello dear visitor, welcome to our site. ');
 }else{
     console.log('Dear visitor, our site unfortunatlu only supports French, Dutch and English. If you entered another langueage the site will not work. \n Be sure to also write the names of the language with a capital in front. \n ex. Dutch, French... ');
