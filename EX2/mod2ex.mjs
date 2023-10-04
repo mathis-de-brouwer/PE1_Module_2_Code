4dadfa8c364a048c02e882d0d2f94e9b0cbb5435
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2023-10-04 09:51:05 +0100

    m2

--- EX2/mod2ex.mjs
+++ EX2/mod2ex.mjs
@@ -5,8 +5,25 @@ const userInput = readline.createInterface({input, output});
 //
 
 //m1
+// if/else
 
+let artist = parseFloat(await userInput.question('Kies een artiest: /n 1. Red Hot Chili Peppers /n 2. AC/DC /n 3. Nirvana /n 4. Dire Straits'));
 
+if(1){
+    console.log('Je koos voor "Red Hot Chili Peppers" /n Ze zijn de artist achter de hit: Black Summer.');
+    if(2){
+        console.log('Je koos voor "AC/DC" /n Ze zijn de artist achter de hit: Thunderstruck.');
+        if(3){
+            console.log('Je koos voor "Nirvana" /n Ze zijn de artist achter de hit: Come As You Are.');
+            if(4){
+                console.log('Je koos voor "Dire Straits" /n Ze zijn de artist achter de hit: Sultans Of Swing.');
+            }
+
+        }
+    }
+}else{
+    console.log('Sorry we do not know this band, please choode a band out of the four we provided.');
+}
 
 
 //e3
